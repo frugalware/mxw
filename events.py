876ee92abce9a3e219e2d4d0ce0828c18a489f22
VMiklos <vmiklos@frugalware.org>
2007-06-26 00:51:13 +0000

    event handlers are now realoadable

---
+++ events.py
@@ -0,0 +1,31 @@
+from config import config
+
+def on_welcome(self, c, e):
+	for i in config.channels:
+		c.join(i)
+
+def on_privmsg(self, c, e):
+	nick = e.source().split("!")[0]
+	command(self, c, nick, nick, e.arguments()[0])
+
+def on_pubmsg(self, c, e):
+	# highlight
+	if e.arguments()[0].startswith(c.get_nickname()):
+		source = e.source().split("!")[0]
+		data = e.arguments()[0][len(c.get_nickname()):].strip()
+		if data[:1] == "," or data[:1] == ":":
+			data = data[1:].strip()
+		command(self, c, source, e.target(), data)
+
+def command(self, c, source, target, data):
+	argv = data.split(' ')
+	ret = []
+	if argv[0] == "reload":
+		self.reload()
+		c.privmsg(target, "%s: reload done" % source)
+	elif argv[0] == ":)":
+		c.privmsg(target, "%s: :D" % source)
+	elif argv[0] == ":D":
+		c.privmsg(target, "%s: lol" % source)
+	else:
+		c.privmsg(target, "%s: '%s' is not a validd command" % (source, argv[0]))
--- mxw2.py
+++ mxw2.py
@@ -2,37 +2,19 @@
 
 from ircbot import SingleServerIRCBot
 from config import config
+import events
 
 class Mxw2(SingleServerIRCBot):
 	def __init__(self):
 		SingleServerIRCBot.__init__(self, [(config.server, config.port)], config.nick, config.realname)
-
-	def on_welcome(self, c, e):
-		for i in config.channels:
-			c.join(i)
-
-	def on_privmsg(self, c, e):
-		nick = e.source().split("!")[0]
-		self.command(c, nick, nick, e.arguments()[0])
-
-	def on_pubmsg(self, c, e):
-		# highlight
-		if e.arguments()[0].startswith(c.get_nickname()):
-			source = e.source().split("!")[0]
-			data = e.arguments()[0][len(c.get_nickname()):].strip()
-			if data[:1] == "," or data[:1] == ":":
-				data = data[1:].strip()
-			self.command(c, source, e.target(), data)
-
-	def command(self, c, source, target, data):
-		argv = data.split(' ')
-		ret = []
-		if argv[0] == ":)":
-			c.privmsg(target, "%s: :D" % source)
-		elif argv[0] == ":D":
-			c.privmsg(target, "%s: lol" % source)
-		else:
-			c.privmsg(target, "%s: '%s' is not a valid command" % (source, cmd))
+		self.reload()
+	
+	def reload(self):
+		reload(events)
+		self.events = {}
+		for i in dir(events):
+			if not i.startswith("__"):
+				self.events[i] = getattr(events, i)
 
 if __name__ == "__main__":
 	mxw2 = Mxw2()
