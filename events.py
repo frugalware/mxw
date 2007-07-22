19757a8123d77eefba3d4e0917f77201ac1ada3f
VMiklos <vmiklos@frugalware.org>
2007-07-22 17:44:38 +0200

    new darcs command

--- events.py
+++ events.py
@@ -346,6 +346,32 @@ def db_search(c, source, target, argv):
 	else:
 		c.privmsg(target, "%s: no matching records" % source)
 
+def darcs(c, source, target, argv):
+	"""gives you a deepcmdline to get a given repo"""
+	repodir = "/home/ftp/pub/other/homepage-ng/darcs/repos"
+	if len(argv) < 1:
+		for root, dirs, files in os.walk(repodir):
+			repos = dirs
+		repos.sort()
+		c.privmsg(target, "%s: the following darcs repos are available: %s. use 'darcs foo' to get a deepcmdline" % (source, ", ".join(repos)))
+		return
+	repo = argv[0]
+	found = False
+	for root, dirs, files in os.walk(repodir):
+		for dir in dirs:
+			if dir == repo:
+				found = True
+	if not found:
+		c.privmsg(target, "%s: no such repo" % source)
+		return
+	else:
+		path = os.path.abspath(os.path.join(repodir, os.readlink(os.path.join(repodir, repo))))
+		if repo.startswith("frugalware-"):
+			local = repo[len("frugalware-"):]
+		else:
+			local = ""
+		c.privmsg(target, "%s: darcs get --partial %s@darcs.frugalware.org:%s %s" % (source, source, path, local))
+
 def git(c, source, target, argv):
 	"""gives you a deepcmdline to clone a given repo"""
 	repodir = "/home/ftp/pub/other/homepage-ng/git/repos"
@@ -548,6 +574,7 @@ class config:
 			"uptime": uptime,
 			"db": db,
 			"search": db_search,
+			"darcs": darcs,
 			"git": git,
 			"anongit": anongit,
 			"imdb": imdb
