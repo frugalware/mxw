cc0d5e937643f6721e6c484772ce452afb9048db
VMiklos <vmiklos@frugalware.org>
2007-08-31 16:16:57 +0200

    one more special char for the dict trigger (German..)

--- events.py
+++ events.py
@@ -692,7 +692,7 @@ class config:
 			(lambda e, argv: e.arguments()[0].lower() == "yepp!"): (lambda c, e, source, argv: fortune(c, e, source, "akii-fun.lines", "Yepp!")),
 			(lambda e, argv: e.arguments()[0].lower() == "yow!"): (lambda c, e, source, argv: fortune(c, e, source, "yow.lines", "Yow!")),
 			(lambda e, argv: re.match("^[0-9.]+[KM]? [a-zA-Z]+ in [a-zA-Z]+$", " ".join(argv[:4]))): (lambda c, e, source, argv: google(c, source, e.target(), argv)),
-			(lambda e, argv: (not e.target().startswith("#") or e.target() == "#debian.hu") and re.match("^[a-z]{2} [a-zA-Z\xdf]+$", " ".join(argv))): (lambda c, e, source, argv: dict(c, source, e.target(), argv))
+			(lambda e, argv: (not e.target().startswith("#") or e.target() == "#debian.hu") and re.match("^[a-z]{2} [a-zA-Z\xdf\xfc]+$", " ".join(argv))): (lambda c, e, source, argv: dict(c, source, e.target(), argv))
 			}
 	highlight_triggers = {
 			(lambda e, argv: re.match(r".*\?$", argv[-1]) and e.target() not in ["#frugalware.dev", "#frugalware"]): (lambda c, e, source, argv: c.privmsg(e.target(), """%s: persze""" % source))
