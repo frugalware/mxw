d632601f3231fedd5bb24aa9bf22e2bf66f12b03
Miklos Vajna <vmiklos@frugalware.org>
2007-10-28 13:28:14 +0100

    client: new example about reconnecting

--- client.py
+++ client.py
@@ -3,4 +3,5 @@ import socket, sys
 client = socket.socket ( socket.AF_UNIX, socket.SOCK_DGRAM )
 client.connect ("mxw2.sock")
 client.send ("""c.privmsg("%s", "%s")""" % (sys.argv[1], sys.argv[2]))
+#client.send ("""c.disconnect("reconnecting")""")
 client.close()
