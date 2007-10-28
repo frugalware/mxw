df60d29bc962cd327b10c6fa618dd96f23dae9d8
Miklos Vajna <vmiklos@frugalware.org>
2007-10-28 13:22:12 +0100

    client: typo fix

--- client.py
+++ client.py
@@ -2,5 +2,5 @@ import socket, sys
 
 client = socket.socket ( socket.AF_UNIX, socket.SOCK_DGRAM )
 client.connect ("mxw2.sock")
-client.send ("""c.privmsg("%s", "%s")""" % (sys.argv[1], sys.argv[2])
+client.send ("""c.privmsg("%s", "%s")""" % (sys.argv[1], sys.argv[2]))
 client.close()
