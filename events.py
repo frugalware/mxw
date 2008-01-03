27cc452ebebc76db2b0fb6a11d2c17b30ecea102
Miklos Vajna <vmiklos@frugalware.org>
2008-01-03 13:03:33 +0100

    hex2ip: work on any webchat

--- events.py
+++ events.py
@@ -915,7 +915,7 @@ def on_join(self, c, e):
 	if e.target() == "#frugalware.dev":
 		cmd = 'c.mode("%s", "+v %s")' % (e.target(), nick)
 		safe_eval(nick, cmd, c)
-	elif e.source().split("@")[1] == "frugalware.elte.hu":
+	elif re.match(r".*[0-9a-f]{8}@.*", e.source()):
 		try:
 			ip = hex2ip(re.sub(r".*([0-9a-f]{8})@.*", r"\1", e.source()))
 			try:
