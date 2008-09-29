2301b4a9f76a0fb061d9cadd5a026cdf04936470
Miklos Vajna <vmiklos@frugalware.org>
2008-09-29 16:23:11 +0200

    add a bullshit generator
    from http://www.dack.com/web/bullshit.html

--- events.py
+++ events.py
@@ -887,6 +887,63 @@ def fortune(c, e, source, file, prefix):
 	sock.close()
 	c.privmsg(e.target(), "%s %s" % (prefix, random.choice(lines).replace("\n", ' ').strip()))
 
+def bullshit(c, source, target, argv):
+	"""Bullshit Generator"""
+	list1 = ["implement", "utilize", "integrate", "streamline",
+			"optimize", "evolve", "transform", "embrace",
+			"enable", "orchestrate", "leverage", "reinvent",
+			"aggregate", "architect", "enhance",
+			"incentivize", "morph", "empower",
+			"envisioneer", "monetize", "harness",
+			"facilitate", "seize", "disintermediate",
+			"synergize", "strategize", "deploy", "brand",
+			"grow", "target", "syndicate", "synthesize",
+			"deliver", "mesh", "incubate", "engage",
+			"maximize", "benchmark", "expedite",
+			"reintermediate", "whiteboard", "visualize",
+			"repurpose", "innovate", "scale", "unleash",
+			"drive", "extend", "engineer", "revolutionize",
+			"generate", "exploit", "transition", "e-enable",
+			"iterate", "cultivate", "matrix", "productize",
+			"redefine", "recontextualize"]
+	list2 = ["clicks-and-mortar", "value-added", "vertical",
+			"proactive", "robust", "revolutionary",
+			"scalable", "leading-edge", "innovative",
+			"intuitive", "strategic", "e-business",
+			"mission-critical", "sticky", "one-to-one",
+			"24/7", "end-to-end", "global", "B2B", "B2C",
+			"granular", "frictionless", "virtual", "viral",
+			"dynamic", "24/365", "best-of-breed", "killer",
+			"magnetic", "bleeding-edge", "web-enabled",
+			"interactive", "dot-com", "sexy", "back-end",
+			"real-time", "efficient", "front-end",
+			"distributed", "seamless", "extensible",
+			"turn-key", "world-class", "open-source",
+			"cross-platform", "cross-media", "synergistic",
+			"bricks-and-clicks", "out-of-the-box",
+			"enterprise", "integrated", "impactful",
+			"wireless", "transparent", "next-generation",
+			"cutting-edge", "user-centric", "visionary",
+			"customized", "ubiquitous", "plug-and-play",
+			"collaborative", "compelling", "holistic",
+			"rich"]
+	list3 = ["synergies", "web-readiness", "paradigms", "markets",
+			"partnerships", "infrastructures", "platforms",
+			"initiatives", "channels", "eyeballs",
+			"communities", "ROI", "solutions", "e-tailers",
+			"e-services", "action-items", "portals",
+			"niches", "technologies", "content", "vortals",
+			"supply-chains", "convergence", "relationships",
+			"architectures", "interfaces", "e-markets",
+			"e-commerce", "systems", "bandwidth",
+			"infomediaries", "models", "mindshare",
+			"deliverables", "users", "schemas", "networks",
+			"applications", "metrics", "e-business",
+			"functionalities", "experiences", "web services",
+			"methodologies"]
+	lists = [list1, list2, list3]
+	c.privmsg(target, "%s: %s" % (source, map(random.choice, lists)))
+
 class config:
 	server = "irc.freenode.net"
 	port = 6667
@@ -959,7 +1016,8 @@ class config:
 			"choose": choose,
 			"integrate": integrate,
 			"isbn": isbn,
-			"tv": tv
+			"tv": tv,
+			"bullshit": bullshit
 			}
 	triggers = {
 			#(lambda e, argv: e.target() == "#frugalware" and " ... " in e.arguments()[0]): (lambda c, e, source, argv: c.privmsg(e.target(), """%s: using "..." so much isn't polite to other users. please consider changing that habit.""" % source)),
---
+++ t/t-bullshit.sh
@@ -0,0 +1,2 @@
+gen_py bullshit
+check "bullshit hu2en" "python bullshit.py|grep -q \"['.*', '.*', '.*']\""
