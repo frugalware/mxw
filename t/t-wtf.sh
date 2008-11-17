78495e1d05063e3fe76b43deee4257504fbd1d64
Miklos Vajna <vmiklos@frugalware.org>
2008-11-17 16:06:12 +0100

    fix up t-wtf.sh

--- t/t-wtf.sh
+++ t/t-wtf.sh
@@ -1,3 +1,3 @@
 gen_py wtf
 check "resolve wtf" "[ \"\$(python wtf.py wtf)\" == \"privmsg: source: WTF: {what,where,who,why} the fuck\" ]"
-check "don't resolve wtff" "[ \"\$(python wtf.py wtff)\" == \"privmsg: source: wtff: nothing appropriate\" ]"
+check "don't resolve wtff" "[ \"\$(python wtf.py wtff)\" == \"privmsg: your search did not match any documents\" ]"
