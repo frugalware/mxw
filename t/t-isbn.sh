0ed8b74c844f4d7d7cba0a5bbe4c315377a54e36
Miklos Vajna <vmiklos@frugalware.org>
2009-08-03 18:51:03 +0200

    fix isbn test

--- t/t-isbn.sh
+++ t/t-isbn.sh
@@ -4,4 +4,4 @@ check "isbn title" "python isbn.py 0156403005|sed -n 1p \
 check "isbn desc" "python isbn.py 0156403005|sed -n 2p \
 |grep -q \"^privmsg: by Stanislaw Lem, Michael Kandel - Fiction - 1984 - 208 pages\$\""
 check "isbn url" "python isbn.py 0156403005|sed -n 3p \
-|grep -q \"^privmsg: http://books.google.com/books?id=uLFQAAAACAAJ&dq=isbn:0156403005\$\""
+|grep -q \"^privmsg: http://books.google.com/books?id=1PRrPwAACAAJ&dq=isbn:0156403005\$\""
